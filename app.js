2cca0b9793e9172d5d607d9c067849a9572e8646
Luiz <luizfernsan@gmail.com>
2025-08-14 21:07:53 -0300

    Funções adicionarAmigo() e formatarNome() criadas

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let amigo = document.getElementById('amigo');
+let lista = document.getElementById('listaAmigos');
+
+function adicionarAmigo() {
+    console.log(typeof amigo.value);
+    if (amigo.value === '' || amigo.value === null || amigo.value === undefined || amigo.value.trim() === '' || amigo.value.length < 3) {
+        alert('Por favor, insira um nome válido.');
+    } else if (amigos.includes(formatarNome(amigo.value))) {
+        alert('Este amigo já foi adicionado.');
+    } else {
+        amigos.push(formatarNome(amigo.value));
+        let item = document.createElement('li');
+        item.textContent = amigos[amigos.length - 1];
+        lista.appendChild(item);
+    }
+    amigo.value = '';
+    amigo.focus();
+}
+
+function formatarNome(str) {
+    // Função para transformar a string com a primeira letra maiúscula e o restante minúsculo
+    if (typeof str !== 'string' || str.length === 0) {
+        return str;
+    }
+    return str
+        .split(' ')
+        .map(palavra => palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase())
+        .join(' ');
+}
