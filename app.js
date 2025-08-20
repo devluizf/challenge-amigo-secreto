57a8be780db0828e68f593e6bfef019f1699ba9a
Luiz <luizfernsan@gmail.com>
2025-08-19 21:17:32 -0300

    Função sortearAmigo() criada

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 let amigos = [];
 let amigo = document.getElementById('amigo');
 let lista = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+let amigoSorteado = null;
 
 function adicionarAmigo() {
-    console.log(typeof amigo.value);
-    if (amigo.value === '' || amigo.value === null || amigo.value === undefined || amigo.value.trim() === '' || amigo.value.length < 3) {
+    if (amigo.value === '' || 
+        amigo.value === null || 
+        amigo.value === undefined || 
+        amigo.value.trim() === '' || 
+        amigo.value.length < 3 ||
+        !isNaN(parseInt(amigo.value))) {
         alert('Por favor, insira um nome válido.');
     } else if (amigos.includes(formatarNome(amigo.value))) {
         alert('Este amigo já foi adicionado.');
@@ -27,4 +33,17 @@ function formatarNome(str) {
         .split(' ')
         .map(palavra => palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase())
         .join(' ');
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Por favor, adicione amigos antes de sortear.');
+    } else if (amigoSorteado !== null) {
+        alert('O sorteio já foi realizado. Atualize a página para sortear novamente.');
+    } else {
+        amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        let itemResultado = document.createElement('li');
+        itemResultado.textContent = `O amigo sorteado é: ${amigoSorteado}`;
+        resultado.appendChild(itemResultado);
+    }
 }
